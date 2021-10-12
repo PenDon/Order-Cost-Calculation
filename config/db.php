066fd9fdc7a41ee4e35d27827eacaa3f6d7eb018
dongpenh <13122995191@163.com>
2021-10-12 10:44:43 +0800

    ...

--- config/db.php
+++ config/db.php
@@ -2,12 +2,10 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=39.108.118.113;dbname=mlyj_email',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=cost_calculation',
 //    'dsn' => 'mysql:host=47.88.23.201;dbname=wordpress',
-    'username' => 'mlyj',
-//    'username' => 'mlyj',
-//    'password' => 'mlyj_peng',
-    'password' => '1101561894dp',
+    'username' => 'root',
+    'password' => 'root',
     'charset' => 'utf8',
     'tablePrefix' => '',
 //    'tablePrefix' => 'wp_',
